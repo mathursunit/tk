b5f3c16121683eb7af7457656931b80dbd2d24bf
mathursunit <sunit.mathur@gmail.com>
2025-12-22 11:47:11 -0500

    Force service worker update with skipWaiting and new cache v3

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('taaza-cache-v2').then(cache => {
+    caches.open('taaza-cache-v3').then(cache => {
       return cache.addAll([
         '/',
         '/index.html',
@@ -8,8 +8,14 @@ self.addEventListener('install', event => {
         '/icon-192.png',
         '/icon-512.png'
       ]);
-    })
+      ]);
+})
   );
+self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
 self.addEventListener('fetch', event => {
   event.respondWith(
