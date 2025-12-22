a3d49f8518c52677062e62f2015b58c3a70257ba
mathursunit <sunit.mathur@gmail.com>
2025-12-22 12:43:11 -0500

    Enhance notifications: click handler + delay test

--- service-worker.js
+++ service-worker.js
@@ -24,3 +24,15 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
+      if (clientList.length > 0) {
+        return clientList[0].focus();
+      }
+      return clients.openWindow('/');
+    })
+  );
+});
